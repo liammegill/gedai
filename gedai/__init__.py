3a6d8517fb821df01d21988c0823edcbd1438712
Liam Megill <liam.megill@dlr.de>
2025-07-04 17:44:55 +0200

    Setup package

---
+++ gedai/__init__.py
@@ -0,0 +1,9 @@
+"""Create the gedai package."""
+
+from .core import *  # F401, F403
+from .fetch import *  # F401, F403
+from .preprocess import *  # F401, F403
+from .fuel import *  # F401, F403
+
+# util functions
+from .utils.legs import *  # F401, F403
