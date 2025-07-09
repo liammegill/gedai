c088ad95f22604e9836f7409996cf4a0085c6957
Liam Megill <liam.megill@dlr.de>
2025-07-09 11:29:43 +0200

    Update package info

--- gedai/__init__.py
+++ gedai/__init__.py
@@ -1,9 +1,10 @@
 """Create the gedai package."""
 
-from .core import *  # F401, F403
-from .fetch import *  # F401, F403
+from .core import *        # F401, F403
+from .fetch import *       # F401, F403
 from .preprocess import *  # F401, F403
-from .fuel import *  # F401, F403
+from .fuel import *        # F401, F403
+from .emissions import *   # F401, F403
 
 # util functions
 from .utils.legs import *  # F401, F403
